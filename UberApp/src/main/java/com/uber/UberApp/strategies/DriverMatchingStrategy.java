bc3f93a535b9583a61bd442fcf1a451250869ad9
Rushi4511 <rushikarle45@gmail.com>
2024-12-07 21:52:18 +0530

    Driver Matching Strategy 1.2

--- UberApp/src/main/java/com/uber/UberApp/strategies/DriverMatchingStrategy.java
+++ UberApp/src/main/java/com/uber/UberApp/strategies/DriverMatchingStrategy.java
@@ -1,4 +1,11 @@
 package com.uber.UberApp.strategies;
 
+import com.uber.UberApp.dto.RideRequestDto;
+import com.uber.UberApp.entities.Driver;
+
+import java.util.List;
+
 public interface DriverMatchingStrategy {
+
+    List<Driver> findMatchingDriver(RideRequestDto rideRequestDto);
 }
