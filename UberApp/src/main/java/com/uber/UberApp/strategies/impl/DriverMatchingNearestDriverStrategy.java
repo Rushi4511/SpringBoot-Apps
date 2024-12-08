2c43fa6b7feb96238045f41b08142d50915fbf30
Rushi4511 <rushikarle45@gmail.com>
2024-12-08 22:32:59 +0530

    Implementation of driver matching strategy 1.3

--- UberApp/src/main/java/com/uber/UberApp/strategies/impl/DriverMatchingNearestDriverStrategy.java
+++ UberApp/src/main/java/com/uber/UberApp/strategies/impl/DriverMatchingNearestDriverStrategy.java
@@ -1,10 +1,18 @@
 package com.uber.UberApp.strategies.impl;
 
+import com.uber.UberApp.dto.RideRequestDto;
+import com.uber.UberApp.entities.Driver;
 import com.uber.UberApp.strategies.DriverMatchingStrategy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DriverMatchingNearestDriverStrategy implements DriverMatchingStrategy {
 
 
+    @Override
+    public List<Driver> findMatchingDriver(RideRequestDto rideRequestDto) {
+        return List.of();
+    }
 }
