e57e221eabcd3a2d5d47c03afe538490163f6c28
Rushi4511 <rushikarle45@gmail.com>
2024-12-09 23:02:52 +0530

    Driver Matching Higest Rated Strategy 1.3

--- UberApp/src/main/java/com/uber/UberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
+++ UberApp/src/main/java/com/uber/UberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
@@ -1,6 +1,14 @@
 package com.uber.UberApp.strategies.impl;
 
+import com.uber.UberApp.dto.RideRequestDto;
+import com.uber.UberApp.entities.Driver;
 import com.uber.UberApp.strategies.DriverMatchingStrategy;
 
+import java.util.List;
+
 public class DriverMatchingHighestRatedDriverStrategy implements DriverMatchingStrategy {
+    @Override
+    public List<Driver> findMatchingDriver(RideRequestDto rideRequestDto) {
+        return List.of();
+    }
 }
